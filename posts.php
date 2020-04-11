c4586c585acc66064ea338c14b795925299aa587
ICE <donfeng97@csu.fullerton.edu>
2020-04-11 01:23:56 -0700

    就差profile了

--- posts.php
+++ posts.php
@@ -18,8 +18,8 @@ session_start();
 // set up connection to database
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error){
-	die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $signal = isset($_POST['signal']) ? $_POST['signal'] : '';
@@ -33,48 +33,128 @@ $userName = "";
     <title>CIRCLE COMMUNITIES</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- 引入 Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- 包括所有已编译的插件 -->
+    <script src="js/bootstrap.js"></script>
 </head>
 
-<body>
+<header>
 
     <div class="header">
-        <?php 
-            echo "<h4>CIRCLE Communities</h4>";
-            if($signal == "guest"){
-                echo "<h5><a href='index.html'> Login / Sign up </a></h5>";
-            }
-            else{
-                // Access Account information from DB
-                $sql = "SELECT UserName From ACCOUNTS WHERE Email='$account'";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                    $row = $result->fetch_assoc();
-                    $userName = $row['UserName'];
-                }
-                else{
-                    echo '<script language="javascript">';
-                    echo 'if(confirm("Cannot Find Your Login Information, Automatically Log Out")) window.location.href="index.html"';
-                    echo '</script>';
-                }
-                // Session information to next page
-                $_SESSION['login_account'] = $account;
-                $_SESSION['signal'] = "login";
-                echo "<h4><a href='profile.php?login_account=". $_SESSION['login_account'] . "&signal=" . $_SESSION['signal'] . "'>$userName</a></h4>";
-                echo "<h6><a href='index.html'> Log out </a></h6>";
-            }
-        ?>  
-    </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" style="font-size: 2.5rem;">CIRCLE Communities</a>
 
-    <div class="display_posts">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                </ul>
+                <?php
+                if ($signal == "guest") {
+                    echo '<button class="btn btn-outline-light my-2 my-sm-0" type="button" href="index.html">Login/Signup</button>';
+                } else {
+                    // Access Account information from DB
+                    $sql = "SELECT UserName From ACCOUNTS WHERE Email='$account'";
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+                        $userName = $row['UserName'];
+                    } else {
+                        echo '<script language="javascript">';
+                        echo 'if(confirm("Cannot Find Your Login Information, Automatically Log Out")) window.location.href="index.html"';
+                        echo '</script>';
+                    }
+                    // Session information to next page
+                    $_SESSION['login_account'] = $account;
+                    $_SESSION['signal'] = "login";
+                    echo "<a class='btn btn-outline-light my-2 my-sm-0' href='profile.php?login_account=" . $_SESSION['login_account'] . "&signal=" . $_SESSION['signal'] . "'>$userName</a>";
+                    echo "<a class='btn btn-outline-light my-2 my-sm-0' href='index.html'> Log out </a>";
+                }
+                ?>
+            </div>
+        </nav>
+
         <?php
+        /*
+        if ($signal == "guest") {
+            echo "<h5><a href='index.html'> Login / Sign up </a></h5>";
+        } else {
+            // Access Account information from DB
+            $sql = "SELECT UserName From ACCOUNTS WHERE Email='$account'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $userName = $row['UserName'];
+            } else {
+                echo '<script language="javascript">';
+                echo 'if(confirm("Cannot Find Your Login Information, Automatically Log Out")) window.location.href="index.html"';
+                echo '</script>';
+            }
+            // Session information to next page
+            $_SESSION['login_account'] = $account;
+            $_SESSION['signal'] = "login";
+            echo "<h4><a href='profile.php?login_account=" . $_SESSION['login_account'] . "&signal=" . $_SESSION['signal'] . "'>$userName</a></h4>";
+            echo "<h6><a href='index.html'> Log out </a></h6>";
+        }*/
+        ?>
+    </div>
+</header>
+
+<body>
+    <div class="body_godown">
+        <div class="container">
+            <label>
+                <h2>Posts</h2>
+            </label>
+                <?php
+                $query = "SELECT * FROM POSTS ORDER BY PostTime DESC;";
+                $stmt = $conn->prepare($query);
+                $stmt->execute();
+                $stmt->store_result();
+                $stmt->bind_result($title, $message, $postTime, $author);
+
+                //echo "<legend>Posts</legend>";
+                echo '<ul class="list-group-flush">';
+                while ($stmt->fetch()) {
+                    // get icon and user name
+                    $icon_path = "";
+                    $image_root = "uploads/";
+                    $sql = "SELECT Path, UserName From ACCOUNTS WHERE Email='$author'";
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+                        $icon_path = $row['Path'];
+                        $userName = $row['UserName'];
+                    } else {
+                        echo '<script language="javascript">';
+                        echo 'if(confirm("Cannot Find Icon path, Automatically Log Out")) window.location.href="index.html"';
+                        echo '</script>';
+                    }
+                    $icon_path = $image_root . basename($icon_path);
+
+                    // Display posts
+                    echo '<li class="list-group-item">';
+                    echo '<div class="card" style=" width: 50rem;">';
+                    echo '<div class="card-body">';
+                    echo '<h5 class="card-title" style="color:#2699FB;" >' . $title . '</h5>';
+                    echo "<h6 class='card-subtitle mb-2 text-muted'>by <img src= '$icon_path' width=10px />$userName at $postTime </h6>";
+                    echo "<p class='card-text'>$message</p>";
+                    echo '</li>';
+                }
+                echo '</ul>';
+                ?>
+
+
+            <?php
+            /*
             $query = "SELECT * FROM POSTS ORDER BY PostTime DESC;";
             $stmt = $conn->prepare($query);
             $stmt->execute();
             $stmt->store_result();
-            $stmt->bind_result($title,$message, $postTime, $author);
+            $stmt->bind_result($title, $message, $postTime, $author);
             echo " <fieldset>";
-            echo "<legend>Posts</legend>";
-            while($stmt->fetch()){
+            //echo "<legend>Posts</legend>";
+            while ($stmt->fetch()) {
                 // get icon and user name
                 $icon_path = "";
                 $image_root = "uploads/";
@@ -84,8 +164,7 @@ $userName = "";
                     $row = $result->fetch_assoc();
                     $icon_path = $row['Path'];
                     $userName = $row['UserName'];
-                }
-                else{
+                } else {
                     echo '<script language="javascript">';
                     echo 'if(confirm("Cannot Find Icon path, Automatically Log Out")) window.location.href="index.html"';
                     echo '</script>';
@@ -100,7 +179,9 @@ $userName = "";
                 echo "----------------------------------------------------------------<br>";
             }
             echo "</fieldset>";
-        ?>
+            */
+            ?>
+        </div>
     </div>
 
 
